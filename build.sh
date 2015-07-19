046f35dffb01ae2d1515bd9af0c5417fead9531e
robodylan <robodylan123@gmail.com>
2015-07-20 04:52:02 +1000

    Made build script more compatible with other distros

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ dd if=/dev/zero of=out/bootable.img count=20 bs=1048576
 sudo parted --script out/bootable.img mklabel msdos mkpart p ext2 1 20 set 1 boot on
 sudo kpartx -a out/bootable.img
 sleep 1
-sudo mkfs.vfat -F 32 /dev/mapper/loop0p1
+sudo mkfs.msdos -F 16 /dev/mapper/loop0p1
 sudo mkdir -p build/tmp/p1
 sudo mount /dev/mapper/loop0p1 build/tmp/p1
 sudo cp -r iso/*  build/tmp/p1
-sudo grub2-install --boot-directory=build/tmp/p1/boot --no-floppy --modules="biosdisk part_msdos fat ext2 configfile normal multiboot" --root-directory=build/tmp/p1 /dev/loop0 
+sudo grub-install --boot-directory=build/tmp/p1/boot --target=i386-pc --no-floppy --modules="biosdisk part_msdos fat ext2 configfile normal multiboot" --root-directory=build/tmp/p1 /dev/loop0 
 sudo umount build/tmp/p1
 sudo kpartx -d out/bootable.img
 qemu-system-i386 -hda out/bootable.img
