a141da4dc5a332efb2cce153f3dcaedd2a5fb2df
robodylan <robodylan123@gmail.com>
2015-07-20 12:29:09 +1000

    Added reading and listing of root directory

--- src/fs.h
+++ src/fs.h
@@ -1,6 +1,17 @@
 void fs_init()
 {
-	char info;
-	hd_rw(2048, HD_READ, 1, &info);
-	write(STDOUT_FILENO, &info, 512);	
+	kprintf("Initializing filesystem...\n");
+	char *info;
+	unsigned int current_sector = 2129;
+	hd_rw(current_sector, HD_READ, 512, info);
+	int i  = 0;
+	kprintf("FAT32 loaded...\n\nRoot Directory ->\n");
+	while(i < 256)
+	{
+		write(STDOUT_FILENO, info, 32);
+		kprintf("\n");
+		i += 64;
+		info += 64;
+	}
+	kprintf("\n");		
 }
--- src/init.c
+++ src/init.c
@@ -18,5 +18,6 @@ void main(void)
 	kprintf("INIT: Entering runlevel 3\n");
 	kb_init();//Init Keyboard
 	fs_init();//Init filesystem
-	cli_init();//Init command line			
+	cli_init();//Init command line
+	while(1==1);//Prevent overflow			
 }
