8c08ca54d8bdbb97bf85361c750395664eb8a220
Dylan Dunn <robodylan123@gmail.com>
2016-03-06 20:41:40 -0500

    Layed out syscalls and added interrupt handler

---
+++ src/sys.h
@@ -0,0 +1,31 @@
+/*
+*  sys.h
+*/
+
+void sys_read(void) //Define handler for system call 1:_read
+	{
+	}
+void sys_write(void)
+	{
+	}
+void sys_open(void)
+	{
+	}
+void sys_close(void)
+	{
+	}
+void sys_exit(void)
+	{
+	}
+void sys_kill(void)
+	{
+	}
+void sys_reboot(void)
+	{
+	}
+void sys_pause(void)
+	{
+	}
+void sys_exec(void)
+	{
+	}
